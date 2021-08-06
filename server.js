d49542bdc4e94ed27367efb1a85447dbc3a61581
BenPot <pot.94.handig@gmail.com>
2021-08-06 16:17:26 +0800

    changing port variable

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ app.get('/profile/:id', profile.handleProfileGet(db));
 app.put('/image', image.handleImage(db));
 app.post('/imageAPI', image.handleAPICall());
 
-app.listen(3000, () => {
-	console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
 })
