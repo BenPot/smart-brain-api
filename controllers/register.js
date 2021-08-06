90d6b3eefa44ea5dd7d058559ec3d22bdef7f301
BenPot <pot.94.handig@gmail.com>
2021-08-06 15:56:03 +0800

    adding starting projects

---
+++ controllers/register.js
@@ -0,0 +1,33 @@
+const handleRegister = (db, bcrypt) => (req, res) => {
+	const { name, email, password } = req.body;
+	if (!name || !email || !pasword) {
+		return res.status(400).json('cannot register');
+	}
+	const hash = bcrypt.hashSync(password);
+	db.transaction(trx => {
+		trx('login')
+			.insert({
+				hash: hash,
+				email: email
+			})
+			.returning('email')
+			.then(loginEmail => {
+				return trx('users')
+					.insert({
+						email: loginEmail[0],
+						name: name,
+						joined: new Date()
+					})
+					.returning('*')
+					.then(user => res.json(user[0]))
+			})
+			.then(trx.commit)
+			.catch(trx.rollback);
+
+	})
+	.catch(err => res.status(400).json('unable to register'));
+}
+
+module.exports = {
+	handleRegister
+}
---
+++ controllers/signin.js
@@ -0,0 +1,23 @@
+const handleSignin = (db, bcrypt) => (req, res) => {
+	const { email, password } = req.body;
+	if (!email || !pasword) {
+		return res.status(400).json('cannot signin');
+	}
+	db.select('*').from('login')
+		.where('email', req.body.email)
+		.then(data => {
+			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+			if (isValid) {
+				return db('users').select('*').where('email', data[0].email)
+					.then(user => res.json(user[0]))
+					.catch(err => res.status(400).json('cannot load the user'));
+			} else {
+				res.json('wrong credentials');
+			}
+		})
+		.catch(err => res.status(400).json('cannot load the user'));
+}
+
+module.exports = {
+	handleSignin
+}
